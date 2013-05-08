7e3f0d2e9afd32cb4a9c7c3520aa1a4cc6d830a7
krunal intwala <krunal.intwala.2@gmail.com>
2013-05-08 17:18:36 -0400

    Corrected work
    uploaded the right work for backbone.js that gets camera objects

--- js/main.js
+++ js/main.js
@@ -143,10 +143,8 @@ $(document).ready(function () {
     app = new AppRouter();
     Backbone.history.start();
 });
+
 /*
-var flickrAPIkey = "48b33ec586d5c4cb6b715d77fa1e8e97";
-
-
 var image = Backbone.Model.extend({
     fullsize_url: function () {
         return this.image_url('medium');
@@ -169,191 +167,38 @@ var image = Backbone.Model.extend({
 
 var imageCollection = Backbone.Collection.extend({
     model: image,
-    key: flickrAPIkey,
+    key: apiKey,
     page: 1,
-    fetch: function (keywords, lat, lon, success) {
-        var self = this;
-        success = success || $.noop;
-        this.keywords = keywords || this.keywords;
-        this.lat = lat || this.lat;
-        this.lon = lon || this.lon;
-        $.ajax({
-            url : 'http://api.flickr.com/services/rest/',
-            data : {
-                api_key : self.key,
-                format : 'json',
-                method : 'flickr.photos.search',
-                tags : this.keywords,
-                has_geo: 1,
-                lat: this.lat,
-                lon: this.lon,
-                accuracy: 11,
-                extras: 'geo',
-                per_page : 30,
-                page : this.page
-            },
-            dataType : 'jsonp',
-            jsonp : 'jsoncallback',
-            success : function (response) {
-                self.add(response.photos.photo);
-                success();
-                //once data succesfully loaded
-                app.changePage(new locationIndexView({collection: self}));
-            }
-        });
-    },
-    nextPage: function (callback) {
-        this.page += 1;
-        this.remove(this.models);
-        this.fetch(null, callback);
-    },
-    prevPage: function(callback) {
-        if (this.page > 1) {this.page -= 1;}
-        this.remove(this.models);
-        this.fetch(null, callback);
+ 
+            fetch: function (str){
+             s = str;
+             //alert(s);
+             $.ajax({
+              url: "http://api.flickr.com/services/rest/?",
+               data: {
+            api_key: apiKey,
+            format: 'json',
+         method: "flickr.cameras.getBrandModels",
+         brand: s,
+       },
+       dataType: 'jsonp',
+       jsonp: 'jsoncallback',
+    success: function(rsp){
+      window.rsp = rsp;
+      console.log(rsp);
+      $("#cameraModels ul").empty();
+      for (var i=0; i<10; i++) {
+          camera = rsp.cameras.camera[i];
+          var brand = rsp.cameras.brand;
+          var model = camera.id;
+          var image = camera.images.small;
+          //var cameraImage="js/bypass.php?image_src="+image;
+          self.add(camera.id);
+          //success();
+          //$("#cameraModels ul").append("<li><a onclick=getPhotos('"+model+"') href=#results><img class='ui-li-icon' src='"+image+"'>"+brand+" "+model+"</a></li>");
+          //$("#cameraModels ul").listview('refresh');
+        app.changePage(new Page6View({collection: self}));
+      }
     }
-});
-
-imageView = Backbone.View.extend({
-    
-    tagName: "div",
-    //className: "imageContainer",
-    template: _.template($('#imageTemplate').html()),
-    
-   
-    intialize: function(){
-        _.bindAll(this,"render");
-        this.collection.bind("all",this.count);
-    },
-    
-    
-    
-});
-
-var imageCollection = new imageCollection();
-
-//router and views
-
-
-window.HomeView = Backbone.View.extend({
-
-    template:_.template($('#home').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-window.geolocationView = Backbone.View.extend({
-    
-    template:_.template($('#geolocation').html()),
-    
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-window.locationIndexView = Backbone.View.extend({
-
-    template: _.template($('#locationIndex').html()),
-    
-    initialize: function(){
-        _.bindAll(this,'render','loadImage');
-        
-    },
-    
-    render: function (eventName) {
-       /* _.each(this.collection.models, function (item) {
-            console.log(item);
-        });
-       
-
-       
-        console.log(this.el);
-        
-        
-       $(this.el).html(this.template());
-       $(this.el).append("<div>");
-       console.log(this.collection.models.length);
-       for(var i = 0; i < this.collection.models.length; i++) {
-            console.log(this.collection.models[i]);
-            var url = this.collection.models[i].image_url('small');
-            $(this.el).append("<div class = 'listImageContainer'><img src = '" + url + "' class = 'listImage'/></div>");
-       }
-       $(this.el).append("</div>")
-        return this;
-    },
-    
-    loadImage: function(item){
-        alert("hi");
-        
-    }
-    
-
-});
-
-window.locationPageView = Backbone.View.extend({
-
-    template:_.template($('#locationPage').html()),
-
-    render:function (eventName) {
-        $(this.el).html(this.template());
-        return this;
-    }
-});
-
-var AppRouter = Backbone.Router.extend({
-
-    routes:{
-        "":"home",
-        "geolocation":"geolocation",
-        "locationIndex":"locationIndex",
-        "locationPage":"locationPage"
-    },
-
-    initialize:function () {
-        // Handle back button throughout the application
-        $('.back').live('click', function(event) {
-            window.history.back();
-            return false;
-        });
-        this.firstPage = true;
-    },
-
-    home:function () {
-        console.log('#home');
-        this.changePage(new HomeView());
-    },
-
-    locationIndex:function () {
-        console.log('#locationIndex');
-        this.changePage(new locationIndexView());
-    },
-    
-    locationPage:function () {
-        console.log('#locationPage');
-        this.changePage(new locationPageView());
-    },
-
-    changePage:function (page) {
-        $(page.el).attr('data-role', 'page');
-        page.render();
-        $('body').append($(page.el));
-        var transition = $.mobile.defaultPageTransition;
-        // We don't want to slide the first page
-        if (this.firstPage) {
-            transition = 'none';
-            this.firstPage = false;
-        }
-        $.mobile.changePage($(page.el), {changeHash:false, transition: transition});
-    }
-
-});
-
-$(document).ready(function () {
-    console.log('document ready');
-    app = new AppRouter();
-    Backbone.history.start();
-});*/
+  });*/
+ 
